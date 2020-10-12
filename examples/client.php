e5992c6390cb56ddf065dae33760526547ce5221
Dominik Meyer <kinimodmeyer@gmail.com>
2020-10-12 10:29:38 +0200

    change credentials to options.

--- examples/client.php
+++ examples/client.php
@@ -1,14 +1,16 @@
 <?php
 require 'vendor/autoload.php';
 
-$credentials = [
-    'account_number' => '1234',
-    'account_user' => 'dom',
-    'account_password' => 'xyz'
+$options = [
+    'credentials' => [
+        'account_number' => '1234',
+        'account_user' => 'xyz',
+        'account_password' => 'xyz'
+    ]
 ];
 
 $client = new Tradebyte\Client(
-    $credentials
+    $options
 );
 
 #$client->getProductHandler();
